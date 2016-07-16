265c01ae814b7e01c5b095bd867688efac408123
MSIITS <Managed.Services@integratedit.com>
2016-07-16 19:45:09 -0400

    Added Get-MailFlowStats
    Added new function that will gather mail statistics for an Office 365
    client.

--- IITS.ps1
+++ IITS.ps1
@@ -182,3 +182,125 @@ function Toggle-ActionCenter
     {
     }
 }
+
+<#
+.Synopsis
+   This procedure will calculate the total messages sent from all users in Office 365.  It can then email the results along with a .csv file for futher data manipulation
+.DESCRIPTION
+   There are 3 parts to this procedure.  The first part connects to Office 365 after requesting the credentials.  The credentials that are used will dictate what tenant information is gathered.  
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-MailFlowStats
+{
+    [CmdletBinding()]
+    [Alias()]
+     Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$false,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Param1,
+        [switch]$errorlog,
+
+        # Param2 help description
+        [int]
+        $Param2
+    )
+
+    Begin
+    {
+        $file_path_csv = "$env:USERPROFILE\desktop\Email_stats_$(get-date -f yyyyMMdd).csv"
+        $shouldemail = Read-Host -Prompt "Do you want to have the results emailed to you along with a CSV attachment? Enter 'Yes' if desired. If no email is required then output is .csv located at $file_path_csv"
+        if($shouldemail -like "yes"){
+        $office365 = Read-Host -Prompt "Do you want to send through Office 365? Enter Yes or No"
+            if($office365 -like "Yes"){
+            $smtpServer = "outlook.office365.com"
+            }
+            Else{
+            $smtpServer = Read-Host -Prompt "Please enter the IP address of an accessible Exchange Server."
+            }
+        $from = read-host -Prompt "Please enter the From address."
+        $to = Read-Host -Prompt "Please enter the To address."
+        }
+        $credential = Get-Credential -Message "Please enter an account that has administrator privleges on the Office 365 tenant."
+        Write-Output "Closing open powershell session: $(Get-PSSession)"
+        Get-PSSession | Remove-PSSession
+        try{
+            $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $credential -Authentication Basic -AllowRedirection -ErrorAction SilentlyContinue -ErrorVariable session_issue
+            Import-PSSession $Session -ErrorAction SilentlyContinue -ErrorVariable $session_issue
+            cls
+            }
+        catch{
+            Write-Output "There was an issue connecting to Office 365 with the credentials supplied.  Please try again or check the error log."
+            $session_issue | Out-File -force "$env:USERPROFILE\desktop\Error_log_$(get-date -f yyyyMMdd).txt"
+            throw
+            }
+    }
+    Process
+    {
+        [int]$days_past = -(Read-Host -Prompt 'Enter the number of days in the past to gather reports. Up to a max of 30 days')
+        $date = get-date
+        $report = @()
+        $entry = 1
+        $recipients = @()
+        Write-Output "Getting recipient list for users with a usermailbox."
+        $recipients = Get-Recipient -RecipientTypeDetails UserMailBox -RecipientType UserMailBox
+        $start_time = Get-Date
+        foreach ($recipient in $recipients)
+        {
+            Write-Output "Currently working on $recipient.  Number $entry of $(($recipients | measure-object).Count) total. $(($entry/$(($recipients | measure-object).Count))*100 -as [int])% Complete"
+            try
+            {
+                $messages_received = Get-MessageTrace -RecipientAddress $recipient.PrimarySMTPAddress -StartDate $date.AddDays($days_past) -EndDate $date | Measure-Object -ErrorAction Stop -ErrorVariable issue
+                $messages_sent = Get-MessageTrace -SenderAddress $recipient.PrimarySMTPAddress -StartDate $date.AddDays($days_past) -EndDate $date | Measure-Object -ErrorAction Stop -ErrorVariable issue
+                $Prop=[ordered]@{
+                            'Display Name'=$recipient.DisplayName
+                            'Start Date'=$date.AddDays($days_past)
+                            'End Date'=$date
+                            'Messages Received'=$messages_received.Count
+                            'Messages Sent'=$messages_sent.Count
+                            'Total Messages'=($messages_received.Count + $messages_sent.Count)
+                            }
+                $report += New-Object -TypeName psobject -Property $Prop
+                $entry = ++$entry
+            }
+            catch
+            {
+                Write-Output "Error with $recipient.  Check error log for exact issue"
+                $issues += $issue
+                continue
+            }
+        }
+    }
+    End
+    {
+        if($shouldemail -like "yes")
+        {
+            $report | Export-Csv -Path $file_path_csv -force
+            $subject = "Script finished in $(((get-date).Subtract($start_time)).TotalMinutes -as [int]) minutes!"
+                try
+                {
+                Send-MailMessage -To $to -Subject $subject -BodyAsHtml "$($report | ConvertTo-Html),$($issues | ConvertTo-Html)" -Credential $credential -SmtpServer $smtpServer -UseSsl -From $from -Attachments $file_path_csv -ErrorAction Stop -ErrorVariable email_error
+                Remove-Item -Path $file_path_csv -Force
+                }
+                catch
+                {
+                Write-Output "Something went wrong while trying to email file.  Defaulting to file output."
+                Write-Output "The report has been generated and is located at $file_path_csv"
+                break
+                }
+        }
+        Else
+        {
+                $report | Export-Csv -Path $file_path_csv -Force
+                Write-Output "The report has been generated and is located at $file_path_csv"
+        }
+    Write-Output "Closing powershell sessions: $(Get-PSSession)"
+    Get-PSSession | Remove-PSSession       
+    }
+}
