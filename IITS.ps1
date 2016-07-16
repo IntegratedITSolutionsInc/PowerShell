3a9d76da46b7aa9d1a4a45a770d83290724150b4
MSIITS <Managed.Services@integratedit.com>
2016-07-16 17:47:32 -0400

    Added Toggle-ActionCenter
    Added new function in main script.  Rewrote segment to use Email-MSalarm
    function instead of relying on send-mailmessage.

--- IITS.ps1
+++ IITS.ps1
@@ -109,3 +109,76 @@ function Email-MSalarm
     {
     }
 }
+
+<#
+.Synopsis
+   This function will enable or disable the windows 10 action center.
+.DESCRIPTION
+   This function checks for the existence of the neccessary registry keys and will create the keys if needed.  Once they are created or verified to be there then it will change the appropriate dword based on user request
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Toggle-ActionCenter
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0
+                   )]
+        [ValidateSet("Enable","Disable")]
+        [String]$Setting
+    )
+
+    Begin
+    {
+        $to = "msalarm@integratedit.com"
+        $from = "script_genie@integatedit.com"
+        $smtpserver = "outlook.office365.com"
+        $regpath = "HKCU:\Software\Policies\Microsoft\Windows\Explorer"
+        $namedword = "DisableNotificationCenter"
+        $output= "$env:temp\actioncenter_IITS.txt"
+        if($Setting -eq "Enable")
+        {
+            $Status = 0
+        }
+        Else
+        {
+            $Status = 1
+        }
+    }
+    Process
+    {
+        try
+        {
+            $machineID  = $(Get-ItemProperty -Path "HKLM:\Software\WOW6432Node\Kaseya\Agent\INTTSL74824010499872" -Name MachineID -ErrorAction Stop -ErrorVariable CurrentError).MachineID
+            if (!(Test-Path $regpath))
+            {
+                "$(Get-Date) - Registry path does not exist." | Out-File -FilePath $output -Force -Append
+                New-Item -Path $regpath -Force -ErrorAction Stop -ErrorVariable CurrentError
+                "$(Get-Date) - Created new key $regpath." | Out-File -FilePath $output -Force -Append
+                New-ItemProperty -Path $regpath -Name $namedword -Value $Status -PropertyType DWORD -Force -ErrorAction Stop -ErrorVariable CurrentError
+                "$(Get-Date) - Created new dword $namedword with value of $Setting." | Out-File -FilePath $output -Force -Append
+            }
+            else
+            {
+                 "$(Get-Date) - Registry path exists." | Out-File -FilePath $output -Force -Append
+                 New-ItemProperty -Path $regpath -Name $namedword -Value $Status -PropertyType DWORD -Force -ErrorAction Stop -ErrorVariable CurrentError
+                 "$(Get-Date) - Set new dword $namedword with value of $Setting." | Out-File -FilePath $output -Force -Append
+            }
+        }
+        Catch
+        {
+            Email-MSalarm -From "Powershell@integratedit.com" -Body $CurrentError -Attachment $output
+        }
+    }
+    End
+    {
+    }
+}
