60f05f838ae0472a875ac64f557b0b1cf9ea85b3
CaptainFluffyToes <DKhan@integratedit.com>
2016-07-20 17:13:42 -0400

    Started beginning of Get-ServiceAccount
    This cmdlet will get the service account for all services on the
    machine.

--- IITS.ps1
+++ IITS.ps1
@@ -424,3 +424,38 @@ function disable-365-account
     Set-MsolUser -UserPrincipalName $mailbox -BlockCredential $true
     
 }
+
+<#
+.Synopsis
+   This cmdlet will find the account used to run all services on the machine. 
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-ServiceAccount
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+    )
+
+    Begin
+    {
+        $servers = get-adcomputers
+    }
+    Process
+    {
+        foreach($server in $servers)
+        {
+        Get-WmiObject Win32_Service | Format-Table Name,startname
+        }
+    }
+    End
+    {
+    }
+}
