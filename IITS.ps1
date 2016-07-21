ecf6a390d64f6a86bfc9d414ce53f1a4245c3c1e
msurtees <msurtees@integratedit.com>
2016-07-21 07:53:33 -0400

    Update IITS.ps1

--- IITS.ps1
+++ IITS.ps1
@@ -424,3 +424,28 @@ function disable-365-account
     Set-MsolUser -UserPrincipalName $mailbox -BlockCredential $true
     
 }
+
+<#
+.Synopsis
+   Get App Version
+.DESCRIPTION
+   Gives you the exact application version for an app in Windows
+.EXAMPLE
+   You will be prompted for the full path and application name, ie:  c:\windows\apppp.exe
+
+#>
+function get-app-version
+{
+
+    Param ([string]$app=(Read-host "FULL path with filename:"))
+
+$VersionString = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($app).Fileversion
+$OutputString = "$Machine $QueryString AppVersion $VersionString"
+
+
+$sep="."
+$parts=$VersionString.split($sep)
+$parts[0]
+
+}
+
