9c3bc3b3df8e88892389f43ee391761d4ddbf480
msurtees <msurtees@integratedit.com>
2016-07-25 08:39:23 -0400

    Update IITS.ps1
    .Synopsis
       Gives user full access rights to another's mailbox BUT removes automap

--- IITS.ps1
+++ IITS.ps1
@@ -556,3 +556,50 @@ function hide-user-from-GAL
     {
     }
 }
+
+<#
+.Synopsis
+   Gives user full access rights to another's mailbox BUT removes automap
+.DESCRIPTION
+   
+.EXAMPLE
+   
+.EXAMPLE
+   
+#>
+function mailbox-rights-no-mapping
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        Param ([string]$owner=(read-host "Enter mailbox owner's email address:"))
+        Param ([string]$user=(read-host "Enter user getting access email address:"))
+    )
+
+    Begin
+    {
+
+    #Connect to client's 365
+    $LiveCred = Get-Credential
+    Import-Module msonline; Connect-MsolService -Credential $livecred
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/?proxymethod=rps -Credential $LiveCred -Authentication Basic -AllowRedirection
+    Import-PSSession $Session
+
+    }
+    Process
+    {
+
+    Add-MailboxPermission -Identity $owner -User $user -AccessRights FullAccess -AutoMapping:$false
+
+    }
+
+    End
+    {
+    }
+}
