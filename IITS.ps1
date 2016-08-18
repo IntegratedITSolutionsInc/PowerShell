f46e6db29740218c428ef00a429ccd10b9ef580a
michel surtees <msurtees@itegratedit.com>
2016-08-18 13:55:11 -0400

    SUCKS

--- IITS.ps1
+++ IITS.ps1
@@ -1650,3 +1650,31 @@ function Get-Projection {
                
         }
     }
+
+        
+    <#
+.Synopsis
+   Disable a user's 365 account
+.DESCRIPTION
+   Will prompt for email address of user and disable after you give the 365 credentials
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function disable-365-account
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param ([string]$mailbox=(read-host "Enter user's email address:"))
+
+    #Connect to client's 365
+    $LiveCred = Get-Credential
+    Import-Module msonline; Connect-MsolService -Credential $livecred
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/?proxymethod=rps -Credential $LiveCred -Authentication Basic -AllowRedirection
+    Import-PSSession $Session
+
+    Set-MsolUser -UserPrincipalName $mailbox -BlockCredential $true
+    
+}
