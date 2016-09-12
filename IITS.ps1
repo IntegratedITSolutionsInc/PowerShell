4aa342b9c277c603c3f1da35d73331f05a1057e6
mrsurtees <msurtees@me.com>
2016-09-12 09:17:44 -0400

    Lock User's PC

--- IITS.ps1
+++ IITS.ps1
@@ -2024,4 +2024,21 @@ function get-iitstaskpswd {
         $output = "$unsecpswd is the password for the account"
         return $unsecpswd
 }
+Function Lock-User-Computer
+{
+<#
+.DESCRIPTION
+	Function to Lock a user's computer remotely
+.SYNOPSIS
+	Function to Lock a computer...if already locked this will do nothing
+#>
+	
+$signature = @"
+[DllImport("user32.dll", SetLastError = true)]
+public static extern bool LockWorkStation();
+"@
+
+$LockComputer = Add-Type -memberDefinition $signature -name "Win32LockWorkStation" -namespace Win32Functions -passthru
+$LockComputer::LockWorkStation() | Out-Null
+}
 
