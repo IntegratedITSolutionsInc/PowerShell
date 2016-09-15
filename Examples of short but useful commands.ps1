78c01077e22e49b12eb9327e4195fdf7b8bae41e
mrsurtees <msurtees@me.com>
2016-09-15 09:01:18 -0400

    Update Examples of short but useful commands.ps1

--- Examples of short but useful commands.ps1
+++ Examples of short but useful commands.ps1
@@ -1,5 +1,5 @@
 #  Get Users based on domain
-get-recipient | where {$_.emailaddresses -match “slipstick.com”}
+get-recipient | where {$_.emailaddresses -match "slipstick.com"}
 
 #  Prevent 365 from automatically mapping Mailboxes that a user has permissions to
 Add-MailboxPermission -Identity ahchm@chartis.com -User npatel@chartis.com -AccessRights FullAccess -AutoMapping:$false
