5bf17dcc94e121bfb04f71f29f84e7cd6cf5cca4
agheek <agheek@integratedit.com>
2016-09-16 13:59:10 -0400

    Created Function Get-ErrorEventLogs
    Function to gather error event logs (app. and system)

--- IITS.ps1
+++ IITS.ps1
@@ -2024,4 +2024,41 @@ function get-iitstaskpswd {
         $output = "$unsecpswd is the password for the account"
         return $unsecpswd
 }
+<#
+.Synopsis - The function collects the System and Application Error logs for the last one month. If the error has occured more than 10 times, it sends an e-mail to ms-alarm
+.Description - The function gets the system and application Error logs that have happened in the last one month, It then finds which errors occured more than 10 times. For the source of that event
+it then collects the newest 10 logs and sends an e-mail to MS alarm
+.example : Get-EventLogs
+#>
+ function Get-ErrorEventLogs {
+    <# Calculates the date that was a month before from today #>
+    $monthago = (Get-date).AddDays(-30)
+    <# Machine name on which the function is running #>
+    $machine = Get-KaseyaMachineID
+    <# Create an array of only 2 objects #>
+    $events = "System","Application"
+    <# $e below is each element of the array #>
+    foreach ($e in $events) {
+        <# gets the error logs of $e be it System or Application after a month and of type Error #>
+        $errorlogs = Get-EventLog -LogName $e -After $monthago -EntryType Error | Where-Object {$_.Source -notlike 'DCOM'}
+        <# All the logs of type $e are collected and grouped together to get the name and count of each #>
+        $counterrors = $errorlogs | Group-Object -Property Source -NoElement | Select-Object Count,Name
+            <# again foreach is used to iterate through the above collected information $c is each row with the count and the name of the event log #>
+            foreach ($c in $counterrors) {
+                <# if the count is more than 10, $e is either system or application and $c is the source of the event #>
+                if ($c.count -gt 10) {
+                $source = $c.name
+                $count = $c.count
+                $logs += "Error event with $source in $e Event logs occured $count times in the last 1 month. Below is one such log (the newest)"
+                $logs += Get-EventLog -LogName $e -EntryType Error -Source $source -Newest 1 | fl | Out-String
+                 }
+               }
+             }
+              if ($logs) {
+                    <# e-mail MSAlarm#>
+                    Email-MSalarm -Body "On $machine the following System and Application Error logs have occured more than 10 times in the last 1 month. Below are the numbers and the logs. $logs" -subject "On $machine Error Event logs since $monthago"
+                    }
+        
+  }
 
+ 
